772bc42b736b9933a512aeafe81320a0208d545a
Narendra Vaniya <narendravaniya99@gmail.com>
2025-08-18 20:10:52 +0530

    css

--- src/components/LessonLayout.js
+++ src/components/LessonLayout.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from 'react-router-dom';
-import '../styles/Lessons.css';
+import './styles/Lessons.css';
 
 const LessonLayout = ({ children }) => {
   const [isSidebarOpen, setIsSidebarOpen] = useState(false);
